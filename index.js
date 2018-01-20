1eefec7762807e5d9bfb11fee4ab3ee8b6cf31ac
Scott Grant <scottgrant@khanacademy.org>
2018-01-20 09:56:43 -0800

    Adds animation to the output gif with a customizable party radius

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ const colours = [
     [255, 105, 104]
 ];
 
+const partyOffset = [];
+const partyRadius = 10;
+colours.forEach((c, colourIndex) => {
+    const x = partyRadius * Math.sin(2 * Math.PI * (-colourIndex / colours.length));
+    const y = partyRadius * Math.cos(2 * Math.PI * (-colourIndex / colours.length));
+    partyOffset.push([Math.round(x), Math.round(y)]);
+});
+
 function imageData(err, pixels) {
     if (err) {
         console.log("Invalid image path..");
@@ -51,7 +59,7 @@ function imageData(err, pixels) {
     const outputFile = fs.createWriteStream(outputFilename);
     gif.pipe(outputFile);
 
-    gif.setDelay(100);
+    gif.setDelay(50);
     gif.setRepeat(0);
     gif.setTransparent("0x00FF00");
     gif.writeHeader();
@@ -59,25 +67,37 @@ function imageData(err, pixels) {
         gif.read();
     });
 
-    colours.forEach(function(c) {
-        var p = [];
+    function getPixelValue(arr, shape, x, y) {
+        if (x < 0 || x >= shape[0] || y < 0 || y >= shape[1]) {
+            return -1;
+        }
 
-        greyscale.forEach(g => {
-            if (g === -1) {
-                p.push(0);
-                p.push(255);
-                p.push(0);
-                p.push(0);
-            } else {
-                if (g < 32) {
-                    g = 32;
+        return result = arr[x + y * shape[0]];
+    }
+
+    colours.forEach(function(c, colourIndex) {
+        const offset = partyOffset[colourIndex];
+        const p = [];
+
+        for (var y = 0; y < shape[1]; y += 1) {
+            for (var x = 0; x < shape[0]; x += 1) {
+                let g = getPixelValue(greyscale, shape, x + offset[0], y + offset[1]);
+
+                if (g === -1) {
+                    p.push(0);
+                    p.push(255);
+                    p.push(0);
+                    p.push(0);
+                } else {
+                    g = g < 32 ? 32 : g;
+
+                    p.push(g * c[0] / 255);
+                    p.push(g * c[1] / 255);
+                    p.push(g * c[2] / 255);
+                    p.push(255);
                 }
-                p.push(g * c[0] / 255);
-                p.push(g * c[1] / 255);
-                p.push(g * c[2] / 255);
-                p.push(255);
             }
-        });
+        }
 
         gif.addFrame(p);
         gif.flushData();
